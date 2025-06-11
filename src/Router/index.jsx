f2c392d0a663ef0926b87dd4daa289cfc7d665b2
wlgggy <jeehyo0202@naver.com>
2025-06-11 16:57:17 +0900

    ★[update] : router

--- src/Router/index.jsx
+++ src/Router/index.jsx
@@ -1,16 +1,22 @@
-import { Route, Routes } from "react-router";
+import { Route, Routes, useParams } from "react-router";
 import Main from "../components/pages/Main";
 import App from "../App";
 import Sub from "../components/pages/Sub";
+import UpdateBoard from "../components/pages/Sub";
 
 function Router() {
   return (
-      <Routes>
-        <Route path="/" element = {<App/>}/>
-        <Route path="/main" element = {<Main/>}/>
-        <Route path="/sub" element = {<Sub/>}/>
-      </Routes>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/main" element={<Main />} />
+      <Route path="/sub/:no" element={<Sub />} />
+    </Routes>
   );
+}
+
+function BoardUpdateWrapper() {
+  const { no } = useParams();
+  return <BoardUpdate no={no} />;
 }
 
 export default Router;
