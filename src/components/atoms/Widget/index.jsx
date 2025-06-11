d4ef3d3e36c9bc9ca5f89df351eaf828caf2e6e2
wlgggy <jeehyo0202@naver.com>
2025-06-11 16:55:54 +0900

    ★[create] : widget★

---
+++ src/components/atoms/Widget/index.jsx
@@ -0,0 +1,15 @@
+// InfoCard.js
+import React from "react";
+import { Card, DateText, TitleText, MoreText } from "./styled";
+
+const InfoCard = ({ date, title }) => {
+  return (
+    <Card>
+      <DateText>{date}</DateText>
+      <TitleText>{title}</TitleText>
+      <MoreText>더보기 →</MoreText>
+    </Card>
+  );
+};
+
+export default InfoCard;
---
+++ src/components/atoms/Widget/styled.js
@@ -0,0 +1,45 @@
+import styled from "styled-components";
+
+export const MainContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 0px 20px ;
+  justify-content: start;  /* 줄 안 아이템들은 왼쪽부터 차곡차곡 */
+  align-items: center;
+  width: 850px;
+`;
+
+
+export const Card = styled.div`
+  width: 230px;
+  height: 200px;
+  background-color: #ffffff;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: start;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
+  border-radius: 12px;
+  padding: 20px;
+`;
+
+export const DateText = styled.p`
+  font-size: 14px;
+  color: #999999;
+  margin: 8px 0;
+`;
+
+export const TitleText = styled.h2`
+  font-size: 20px;
+  font-weight: bold;
+  color: #333333;
+  margin: 8px 0;
+  text-align: start;
+`;
+
+export const MoreText = styled.span`
+  font-size: 14px;
+  color: #007bff;
+  margin-top: auto;
+  cursor: pointer;
+`;
