a8e5a096af1fe96f3726d4313557bde923ab7620
wlgggy <jeehyo0202@naver.com>
2025-06-11 16:51:57 +0900

    ★[create] : main★

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import { Link } from 'react-router-dom'
 function App() {
   return (
     <>
-      <h1>바오밥나무 일지</h1>
-      <div className="card">
+      <div>
+        <h1>바오밥나무 일지</h1>
         <button>
           <Link to="/main">시작하기</Link>
         </button>
       </div>
     </>
+
   )
 }
 
