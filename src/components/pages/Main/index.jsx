8a27fabac49d4025bae31b619cadbf3708b407c5
wlgggy <jeehyo0202@naver.com>
2025-06-11 16:56:55 +0900

    ★[update] : main★

--- src/components/pages/Main/index.jsx
+++ src/components/pages/Main/index.jsx
@@ -1,38 +1,54 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import InfoCard from "../../atoms/Widget/index";
+import { MainContainer } from "../../atoms/Widget/styled";
+import Category from "../../atoms/Category";
+import Header from "../../organisms/Header";
 
 function Main() {
-    const [result, setResult] = useState([]); // 초기값은 배열로 설정
+    const [result, setResult] = useState([]);
 
     useEffect(() => {
         const fetchData = async () => {
             try {
                 const response = await axios.get("http://localhost:8080/board");
-                setResult(response.data); // 데이터 상태에 저장
+                setResult(response.data);
             } catch (error) {
                 console.error("데이터를 불러오는 중 오류 발생:", error);
             }
         };
 
-        fetchData(); // 함수 호출
+        fetchData();
     }, []);
 
     return (
-        <div>
-            <h1>게시판 목록</h1>
-            {result.length > 0 ? (
-                result.map((item, index) => (
-                    <div key={index}>
-                        <Link to='/sub'>
-                        <span style={{ width: '200px', display: 'inline-block' }}>{item.title}</span></Link>
-                        <span style={{ width: '100px', display: 'inline-block' }}></span>
-                        <span style={{ width: '500px', display: 'inline-block', textAlign: 'left' }}>{item.content}</span>
+        <div className="Real">
+            <Header/>
+            <div className="yapyap">
+                <div className="Container">
+                    <div className="yap">
+                        <Category title="DESIGN"/>
+                        <Category title="SOFTWARE DEVELOPMENT"/>
+                        <Category title="WEB DEVELOPMENT"/>
+                        <Category title="APP DEVELOPMENT"/>
+                        <Category title="DEVELOPMENT"/>
                     </div>
-                ))
-            ) : (
-                <p>불러올 데이터가 없습니다.</p>
-            )}
+                    <MainContainer>
+                        {result.length > 0 ? (
+                            result.map((item) => (
+                                <div key={item.no} style={{ marginBottom: '20px' }}>
+                                    <Link to={`/sub/${item.no}`}>
+                                        <InfoCard date={item.date} title={item.title} />
+                                    </Link>
+
+                                </div>
+                            ))
+                        ) : (
+                            <p>불러올 데이터가 없습니다.</p>
+                        )}
+                    </MainContainer>
+                </div></div>
         </div>
     );
 }
